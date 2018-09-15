fc3b7b9dcd259fa20c806be23ce976ad8d696ab4
Luiz Cesario <luiz.cesario@yahoo.com.br>
2018-09-14 23:47:25 -0300

    Mostrando a mensagem de sucesso com thymeleaf

--- src/main/java/com/lacsystem/cobranca/controller/TituloController.java
+++ src/main/java/com/lacsystem/cobranca/controller/TituloController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.lacsystem.cobranca.model.Titulo;
 import com.lacsystem.cobranca.repository.Titulos;
@@ -26,12 +27,13 @@ public class TituloController {
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Titulo titulo) {
+	public ModelAndView salvar(Titulo titulo) {
 		
 		
-		System.out.println("titulo:" + titulo.getDescricao());
 		titulos.save(titulo);
-		return "CadastroTitulo";
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem", "Título salvo com sucesso!");
+		return mv;
 	}
 
 }
