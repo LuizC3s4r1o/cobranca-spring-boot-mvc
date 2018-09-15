1ac0ea119f3b209fecb94e06cbb4f15f0c2acecb
Luiz Cesario <luiz.cesario@yahoo.com.br>
2018-09-15 00:09:11 -0300

    Mostrando o combo dinamicamente com thymeleaf

--- src/main/java/com/lacsystem/cobranca/controller/TituloController.java
+++ src/main/java/com/lacsystem/cobranca/controller/TituloController.java
@@ -1,11 +1,16 @@
 package com.lacsystem.cobranca.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.lacsystem.cobranca.model.StatusTitulo;
 import com.lacsystem.cobranca.model.Titulo;
 import com.lacsystem.cobranca.repository.Titulos;
 
@@ -22,18 +27,22 @@ public class TituloController {
 	private Titulos titulos;
 	
 	@RequestMapping("/novo")
-	public String novo() {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public ModelAndView salvar(Titulo titulo) {
-		
-		
 		titulos.save(titulo);
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem", "Título salvo com sucesso!");
 		return mv;
+	}
+	
+	@ModelAttribute("carregarStatusTitulo")
+	public List<StatusTitulo> carregarStatusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
 	}
 
 }
